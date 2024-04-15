258ce894d130f17236c109941c434925b594e7a6
Gemagit <gemitaphotographer@gmail.com>
2024-04-15 10:48:42 +0200

    post/put/delete_adsV1

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,7 +1,50 @@
 const users = require('../models/admin.model');
+const Ads= require('../models/ads.model');
 
 // Controllers básicos CRUDE
 
+//CREATE ads
+
+const createAds = async (req, res) => {
+    try {
+        const data = req.body;
+        let answer = await new Ads(data).save();
+        res.status(201).json(answer);
+    }
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(400).json({ msj: `ERROR: ${error.stack}` });
+    }
+}
+
+//UPDATE ads
+
+const updateAds = async (req, res) => {
+    try {
+        const time = req.params.time;
+        const newData = req.body;
+        let ads = await Ads.updateOne({time}, newData)
+        res.status(200).json(ads);
+    } 
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(400).json({ msj: `ERROR: ${error.stack}` });
+    }
+}
+
+//DELETE ads
+
+const deleteAds = async (req, res) => {
+    try {
+        const newData = req.body;
+        let ads = await Ads.deleteOne(newData)
+        res.status(200).json(ads);
+    } 
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+        res.status(400).json({ msj: `ERROR: ${error.stack}` });
+    }
+}
 
 // Faltan cosas revisar
 // createUser (cuando se registra)
@@ -25,6 +68,7 @@ const users = require('../models/admin.model');
 // deleteAdmin
 
 module.exports = {
-
-
+    createAds,
+    updateAds,
+    deleteAds
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
+const app = express();
 
-const usersController = require("../controllers/users.controller.js");
-const favoritesController = require("../controllers/favorites.controller.js");
+//const usersController = require("../controllers/users.controller.js");
+//const favoritesController = require("../controllers/favorites.controller.js");
 
 
 // [GET] /signup - También la tiene el admin
