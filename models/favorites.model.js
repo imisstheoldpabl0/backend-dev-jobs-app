f07226ce2bbdcb27ceff5462c980be9b5f9f80b0
Gemagit <gemitaphotographer@gmail.com>
2024-04-18 10:04:13 +0200

    createFavorites

--- models/favorites.model.js
+++ models/favorites.model.js
@@ -1,33 +1,23 @@
 const {Pool} = require('pg');
 const pool = require('../config/db_pgsql');
 
-const favorites = require('./queries/favorites.queries')
 
-// CREATE - POST methods
-
-// READ - GET methods
-
-// UPDATE - PUT methods
-
-// DELETE - DELETE methods
-
-
-/* Ejemplo de modelo de otro ejercicio
-
-const getAuthorsByEmail = async (email) => {
-    let client, result;
-    try {
-      client = await pool.connect(); // Espera a abrir conexion
-      const data = await client.query(queries.getAuthorsByEmail, [email]);
-      result = data.rows;
-    } catch (err) {
-      console.log(err);
+const addFavorite = async (id_user, id_job_offer) => {
+  let client;
+  try {
+      client = await pool.connect(); // Abro la conexión con la base de datos
+      // Realiza la operación para añadir la oferta favorita al usuario
+      const query = 'INSERT INTO favorites (id_user, id_job_offer) VALUES ($1, $2)';
+      const values = [id_user, id_job_offer];
+      await client.query(query, values);
+  } catch (err) {
+      console.error(err);
       throw err;
-    } finally {
-      client.release();
-    }
-    return result;
-  }; */
+  } 
+};
 
+module.exports = {
+  addFavorite
+};
 
-modules.exports = favorites;
+//modules.exports = favorites;
